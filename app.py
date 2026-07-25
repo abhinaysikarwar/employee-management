ec160b9a5b88a39abcef16648c05a2abb89882c7
Abhinay Sikarwar <sikarwarlakhan00@gmail.com>
2026-07-25 11:28:51 +0000

    update

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# MongoDB Connection
 client = MongoClient("mongodb://mongodb:27017/")
 db = client["EmployeeDB"]
 collection = db["employees"]
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -13,16 +15,21 @@ def home():
     if request.method == "POST":
 
         employee = {
-            "name": request.form["name"],
-            "age": int(request.form["age"]),
-            "department": request.form["department"]
+            "full_name": request.form["fullname"],
+            "father_name": request.form["fathername"],
+            "qualification": request.form["qualification"],
+            "mobile": request.form["mobile"],
+            "email": request.form["email"],
+            "address": request.form["address"]
         }
 
         collection.insert_one(employee)
 
-        print("Employee Added:", employee)
+        print("Customer Registered Successfully")
+        print(employee)
 
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
